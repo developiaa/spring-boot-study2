c403b442844a1d9ad4de4f8caae58f6c9ce6bebb
developiaa <developiaa@gmail.com>
2023-06-11 18:34:26 +0900

    first commit

---
+++ server/src/main/java/hello/container/MyContainerInitV2.java
@@ -0,0 +1,29 @@
+package hello.container;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HandlesTypes;
+
+import java.util.Set;
+
+// 서블릿 컨테이너 초기화
+@HandlesTypes(AppInit.class) // 초기화 인터페이스 지정
+public class MyContainerInitV2 implements ServletContainerInitializer {
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+        System.out.println("MyContainerInitV2.onStartup");
+        System.out.println("MyContainerInitV2 c = " + c);
+        System.out.println("MyContainerInitV2 ctx = " + ctx);
+
+        for (Class<?> appInitClass : c) {
+            try {
+                // new AppInitV1Servlet()과 같은 코드
+                AppInit appInit = (AppInit) appInitClass.getDeclaredConstructor().newInstance();
+                appInit.onStartup(ctx);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
