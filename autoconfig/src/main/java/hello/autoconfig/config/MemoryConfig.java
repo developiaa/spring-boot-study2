9702e58d20725f5e9c6a72d1164e4a4cd997e221
developiaa <developiaa@gmail.com>
2023-07-09 15:13:08 +0900

    feat: autoconfig: @Conditional #2

--- autoconfig/src/main/java/hello/autoconfig/config/MemoryConfig.java
+++ autoconfig/src/main/java/hello/autoconfig/config/MemoryConfig.java
@@ -3,12 +3,14 @@ package hello.autoconfig.config;
 import hello.memory.MemoryCondition;
 import hello.memory.MemoryController;
 import hello.memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Conditional(MemoryCondition.class)
+//@Conditional(MemoryCondition.class)
+@ConditionalOnProperty(name = "memory", havingValue = "on")
 public class MemoryConfig {
     @Bean
     public MemoryController memoryController() {
