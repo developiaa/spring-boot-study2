a93d4a7a6f50334a5f4c178ef92aa24d587fb074
developiaa <developiaa@gmail.com>
2023-07-31 22:38:18 +0900

    feat: external-read 모듈 추가

---
+++ external-read/src/test/java/hello/externalread/ExternalReadApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.externalread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
