491615b0a237225a957e7f937043b0c9c16fdf25
developiaa <developiaa@gmail.com>
2023-07-31 23:10:15 +0900

    feat: external-read: environment 사용

--- external-read/src/main/java/hello/externalread/ExternalReadApplication.java
+++ external-read/src/main/java/hello/externalread/ExternalReadApplication.java
@@ -1,9 +1,12 @@
 package hello.externalread;
 
+import hello.externalread.config.MyDataSourceEnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(MyDataSourceEnvConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.externalread.datasource")
 public class ExternalReadApplication {
 
 	public static void main(String[] args) {
