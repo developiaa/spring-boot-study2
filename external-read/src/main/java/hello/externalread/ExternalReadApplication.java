d705464f451b7206d9d9438c28b3d56255f5a042
developiaa <developiaa@gmail.com>
2023-08-05 15:24:50 +0900

    feat: external-read: @ConfigurationProperties

--- external-read/src/main/java/hello/externalread/ExternalReadApplication.java
+++ external-read/src/main/java/hello/externalread/ExternalReadApplication.java
@@ -1,14 +1,18 @@
 package hello.externalread;
 
+import hello.externalread.config.MyDataSourceConfigV1;
 import hello.externalread.config.MyDataSourceEnvConfig;
 import hello.externalread.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
 //@Import(MyDataSourceEnvConfig.class)
-@Import(MyDataSourceValueConfig.class)
+//@Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourceConfigV1.class)
 @SpringBootApplication(scanBasePackages = "hello.externalread.datasource")
+@ConfigurationPropertiesScan
 public class ExternalReadApplication {
 
 	public static void main(String[] args) {
