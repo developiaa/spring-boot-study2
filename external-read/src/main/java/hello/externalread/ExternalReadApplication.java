f1fd1d026bac3de8686c5369a7f30e377808d64d
developiaa <developiaa@gmail.com>
2023-08-05 15:00:27 +0900

    feat: external-read: @Value

--- external-read/src/main/java/hello/externalread/ExternalReadApplication.java
+++ external-read/src/main/java/hello/externalread/ExternalReadApplication.java
@@ -1,11 +1,13 @@
 package hello.externalread;
 
 import hello.externalread.config.MyDataSourceEnvConfig;
+import hello.externalread.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(MyDataSourceEnvConfig.class)
+//@Import(MyDataSourceEnvConfig.class)
+@Import(MyDataSourceValueConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.externalread.datasource")
 public class ExternalReadApplication {
 
