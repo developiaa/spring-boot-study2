a6f717281f5efb986884aa3b2a58419f4070d2ac
developiaa <developiaa@gmail.com>
2023-08-13 00:54:38 +0900

    feat: external-read: @ConfigurationProperties 생성자

--- external-read/src/main/java/hello/externalread/ExternalReadApplication.java
+++ external-read/src/main/java/hello/externalread/ExternalReadApplication.java
@@ -1,6 +1,7 @@
 package hello.externalread;
 
 import hello.externalread.config.MyDataSourceConfigV1;
+import hello.externalread.config.MyDataSourceConfigV2;
 import hello.externalread.config.MyDataSourceEnvConfig;
 import hello.externalread.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.Import;
 
 //@Import(MyDataSourceEnvConfig.class)
 //@Import(MyDataSourceValueConfig.class)
-@Import(MyDataSourceConfigV1.class)
+//@Import(MyDataSourceConfigV1.class)
+@Import(MyDataSourceConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.externalread.datasource")
 @ConfigurationPropertiesScan
 public class ExternalReadApplication {
