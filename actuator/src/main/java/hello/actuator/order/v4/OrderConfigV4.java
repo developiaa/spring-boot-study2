0ca8e2e3958db3cc5f1dfc807e108822c10ef61a
developiaa <developiaa@gmail.com>
2023-09-06 22:57:13 +0900

    feat: actuator: 메트릭 등록4 @Timed

---
+++ actuator/src/main/java/hello/actuator/order/v4/OrderConfigV4.java
@@ -0,0 +1,23 @@
+package hello.actuator.order.v4;
+
+import hello.actuator.order.OrderService;
+import hello.actuator.order.v3.OrderServiceV3;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderConfigV4 {
+
+    @Bean
+    OrderService orderService() {
+        return new OrderServiceV4();
+    }
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry meterRegistry) {
+        return new TimedAspect(meterRegistry);
+    }
+
+}
